ed8abd3cca90b7f91823a25dcf5841b1c40f34ed
97Sebas <09sebcor@gmail.com>
2020-09-11 18:43:41 -0600

    esto es una prueba

--- Deteccion/REC.py
+++ Deteccion/REC.py
@@ -1,9 +1,7 @@
-# This script will detect faces via your webcam.
-# Tested with OpenCV3
-
 import cv2
 
 cap = cv2.VideoCapture(0)
+
 # Create the haar cascade
 faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 fullbodyCascade = cv2.CascadeClassifier("haarcascade_fullbody.xml")
@@ -15,11 +13,7 @@ NotDe=0
 while(True):
 	# Capture frame-by-frame
 	ret, frame = cap.read()
-<<<<<<< HEAD
-	frame = cv2.resize(frame, (144, 256))  
-=======
-	frame = cv2.resize(frame, (480, 320))  
->>>>>>> d23e668a9e87749d6326c96d89b4c8636a65c9d0
+	frame = cv2.resize(frame, (320, 240))
 
 	# Our operations on the frame come here
 	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -72,11 +66,7 @@ while(True):
 	
 	if cont1>0 or cont2>0 or cont3>0 :
 		stime+=1
-<<<<<<< HEAD
 		if stime>2:
-=======
-		if stime>4:
->>>>>>> d23e668a9e87749d6326c96d89b4c8636a65c9d0
 			f= open("Movimiento.txt","w+")
 			f.write("1")
 			f.close() 
@@ -86,7 +76,7 @@ while(True):
 			
 	else:
 		NotDe+=1
-		if NotDe > 80:
+		if NotDe > 100:
 			f= open("Movimiento.txt","w+")
 			f.write("0")
 			f.close() 
